f768c1ae7f05435fd9d7496a986d1366d8636b68
tejas2306 <tejasang.spt@gmail.com>
2021-10-11 18:51:36 +0530

    Changed Whole UI

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,6 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         title: Text(widget.title),
       ),
       body:const  Center(
